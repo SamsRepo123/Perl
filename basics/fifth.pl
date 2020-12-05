8394ddb48430c01f81a60bf811c9f51baa6454f2
Articfox <59283331+SamsRepo123@users.noreply.github.com>
2020-12-05 18:24:11 +0530

    Operations using different Operators

---
+++ basics/fifth.pl
@@ -0,0 +1,23 @@
+#
+#  Operators in perl have similar to other languages
+#
+# General operator
+#The concatenation operator (.)
+
+#Example
+
+$var1 = 5;
+$var2 = 6;
+print $var1 + $var2;
+print"\n";
+
+$var ="ab" x 4;
+print $var;
+print"\n";
+
+$var="hack";
+$var=$var . 's';
+print $var 
+
+#
+#

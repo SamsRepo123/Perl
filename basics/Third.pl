d2224827155e7df13a915b3c1ec360df8eb759cb
Articfox <59283331+SamsRepo123@users.noreply.github.com>
2020-12-05 18:15:40 +0530

    Taking input from user

---
+++ basics/Third.pl
@@ -0,0 +1,13 @@
+
+#Taking user input in perl
+
+#Diamond operator is used (<>) to take input ,it is similar to scanf function in C language.
+
+#EXAMPLE
+
+print'Enter Name: ';
+
+$username=<>; # The user will enter text which will be fed into scalar
+
+print "Hello $username  Welcome to Perl Programming language"; 
+

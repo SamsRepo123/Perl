2e346ffa02bc1f7b74aac55c42e6b958dcd2fd82
Articfox <59283331+SamsRepo123@users.noreply.github.com>
2020-12-05 18:12:40 +0530

    This is to show how variables works with print statements

---
+++ basics/second.pl
@@ -0,0 +1,19 @@
+
+#variable declaration
+# $varablename
+
+#EXAMPLE
+$scalarvar='Hello Sam';#The variable scalarvar has value 'Hello Sam'
+
+ 
+# Difference B/w single('') inverted and Double("") inverted commas in print statement
+
+#SiNGLE INVERTED COMMA
+
+print '$scalarvar';   # This prints string i.e ('single inverted comma is used')
+
+print "\n";
+
+#DOUBLE INVERTED COMMA
+
+print "$scalarvar";   # This print value inside variable i.e ("Double inverted comma is used")
